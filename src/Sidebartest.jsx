89d74f667531303747d5556dc2421ee76edd94c5
Manolo Cabrera <viprestal@hotmail.com>
2023-04-26 18:35:30 +0200

    reset

--- src/Sidebartest.jsx
+++ src/Sidebartest.jsx
@@ -66,7 +66,7 @@ const Sidebartest = () => {
               <span className="align-middle px-2 text-base">Dashboard</span>
             </a>
 
-            <ul className="sidebar-dropdown-list-unstyled">
+            <ul className="sidebar-dropdown pl-0 list-none">
               <li className="sidebar-item">
                 <a href="" className="sidebar-link">
                   Analytics
@@ -86,10 +86,10 @@ const Sidebartest = () => {
           </li>
 
           <li className="sidebar-item active">
-            <a href="" className="sidebar-link">
-              <i class="align-middle" data-feather="layout"></i> <span class="align-middle">Pages</span>
+            <a href="" className="sidebar-link list-item">
+              <svg xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-layout align-middle inline-block"><rect x="3" y="3" width="18" height="18" rx="2" ry="2"></rect><line x1="3" y1="9" x2="21" y2="9"></line><line x1="9" y1="21" x2="9" y2="9"></line></svg> <span class="align-middle">Pages</span>
             </a>
-            <ul id="pages" className="sidebar-dropdown-list-unstyled">
+            <ul id="pages" className="sidebar-dropdown pl-0 list-none">
               <li className="sidebar-item"><a href="">Settings</a></li>
               <li className="sidebar-item"><a href="">Projects</a></li>
             </ul>
